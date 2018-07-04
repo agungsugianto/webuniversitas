c29843bd87d9f62bc399325d92f93884dcd7e643
agungsugianto <agungsugianto093@gmail.com>
2018-07-04 12:38:48 +0300

    template

---
+++ app/Http/Controllers/FakultasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\fakultas;
+use Illuminate\Http\Request;
+
+class FakultasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\fakultas  $fakultas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(fakultas $fakultas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\fakultas  $fakultas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(fakultas $fakultas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\fakultas  $fakultas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, fakultas $fakultas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\fakultas  $fakultas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(fakultas $fakultas)
+    {
+        //
+    }
+}
